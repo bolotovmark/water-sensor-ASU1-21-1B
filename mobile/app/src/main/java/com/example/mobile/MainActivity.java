7357b5e5136939cd0f1e1fa3ed8860b6c60ab379
evelinaazmanova <azmanova.evelina.r@gmail.com>
2024-12-13 15:23:59 +0500

    Переписана обработка исключений

--- mobile/app/src/main/java/com/example/mobile/MainActivity.java
+++ mobile/app/src/main/java/com/example/mobile/MainActivity.java
@@ -1,57 +1,64 @@
 package com.example.mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import java.net.*;
-import java.io.*;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     int id = 100;
-    TextView tv;
-    Button btn_on;
-    Button btn_off;
+    TextView tv=findViewById(R.id.textView);
+    TextView tv2=findViewById(R.id.textView2);
+    Button btn_on = findViewById(R.id.button2);
+    Button btn_off = findViewById(R.id.button);
+    Boolean isConnection=true;
+    String ServerAdress="http://192.168.43.42:8080"; ////как пример
+    String cashData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.textView);
-        btn_on = findViewById(R.id.button2);
-        btn_off = findViewById(R.id.button);
+        btn_on.setOnClickListener(v -> new OnOffSender().execute("on"));
 
-        btn_on.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new OnOffSender().execute("on");
-            }
-        });
-
-        btn_off.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new OnOffSender().execute("off");
-            }
-        });
-
+        btn_off.setOnClickListener(v -> new OnOffSender().execute("off"));
 
         createNotificationChannel();
 
+        ShowConnectionOn();
+
         RequestSender RC = new RequestSender();
         RC.execute();
+    }
+    private void ShowConnectionOff()
+    {
+        tv2.setText("Подключение отсутствует");
+        tv2.setTextColor(Color.RED);
+        isConnection=false;
+    }
+    private void ShowConnectionOn()
+    {
+        tv2.setText("Подключено");
+        tv2.setTextColor(Color.GREEN);
+        isConnection=true;
     }
 
     private void createNotificationChannel() {
@@ -66,27 +73,40 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
     }
+
     сlass OnOffSender extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(String... strings) {
-            try {
-                URL url = new URL("http://192.168.43.42:8080");
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("POST");
-                con.setRequestProperty("Content-Type", "text/html");
-                con.setDoOutput(true);
 
-                DataOutputStream dStream = new DataOutputStream(con.getOutputStream());
-                dStream.writeBytes(strings[0]);
-                dStream.flush();
-                dStream.close();
+            cashData=strings[0];
+            if (!isConnection) return null;
 
-            } catch (Exception e) {
+            boolean endOperation=false;
+            while (!endOperation) {
+                try {
+                    URL url = new URL(ServerAdress);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Content-Type", "text/html");
 
+                    con.setDoOutput(true);
+                    DataOutputStream dStream = new DataOutputStream(con.getOutputStream());
+                    dStream.writeBytes(cashData);
+                    dStream.flush();
+                    dStream.close();
+
+                    ShowConnectionOn();
+                    endOperation=true;
+
+
+                } catch (Exception e) {
+                    ShowConnectionOff();
+                    try {TimeUnit.SECONDS.sleep(3);} catch (InterruptedException ignored) {}
+                }
             }
             return null;
         }
-    }
+
 
 
     class RequestSender extends AsyncTask<Void, String, Void> {
@@ -94,29 +114,25 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids)
         {
             while (true) {
-                String data = "";
+                StringBuilder data = new StringBuilder();;
                 try {
-                    URL url = new URL("http://192.168.43.42:8080");
+                    URL url = new URL(ServerAdress);
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     con.setRequestMethod("GET");
                     con.setRequestProperty("Content-Type", "text/html");
                     BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
                     String inputLine;
                     while ((inputLine = in.readLine()) != null) {
-                        data += inputLine;
+                        data.append(inputLine)
                     }
                     in.close();
+                    ShowConnectionOn();
                 } catch (Exception e) {
-                    data = "error " + e.toString();
+                    ShowConnectionOn();
                 }
-                publishProgress(data);
+                publishProgress(data.toString());
 
-                try {
-                    TimeUnit.SECONDS.sleep(5);
-                } catch (InterruptedException e) {
-                    data = "error " + e.toString();
-                    publishProgress(data);
-                }
+                try {TimeUnit.SECONDS.sleep(5);} catch (InterruptedException ignored) {}
             }
         }
 
@@ -124,25 +140,15 @@ public class MainActivity extends AppCompatActivity {
         protected void onProgressUpdate(String... messages) {
             super.onProgressUpdate(messages[0]);
 
-            if (messages[0].length() >= 5 && messages[0].startsWith("error")) {
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "101")
-                        .setSmallIcon(R.drawable.warningicon)
-                        .setContentTitle("Ошибка")
-                        .setContentText(messages[0])
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
-                notificationManager.notify(100, builder.build());
-            }
-            else if (messages[0].equals("w")) {
+            if (messages[0].equals("w")) {
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "101")
                         .setSmallIcon(R.drawable.signalicon)
                         .setContentTitle("ВНИМАНИЕ")
                         .setContentText("сработала сигнализация")
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-                id+=1;
-                if (id>200) id=101;
+                id += 1;
+                if (id > 200) id = 101;
 
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
                 notificationManager.notify(id, builder.build());
