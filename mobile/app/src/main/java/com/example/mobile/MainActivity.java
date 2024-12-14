6ebb766fb8b1bf4beba5b849c67df98e23da15d8
evelinaazmanova <azmanova.evelina.r@gmail.com>
2024-12-14 11:16:09 +0500

    небольшое исправление ошибки

--- mobile/app/src/main/java/com/example/mobile/MainActivity.java
+++ mobile/app/src/main/java/com/example/mobile/MainActivity.java
@@ -15,60 +15,68 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
     int id = 100;
-    TextView tv=findViewById(R.id.textView);
-    TextView tv2=findViewById(R.id.textView2);
-    Button btn_on = findViewById(R.id.button2);
-    Button btn_off = findViewById(R.id.button);
-    Boolean isConnection=true;
-    String ServerAddress ="http://192.168.43.42:8080";
+    TextView tv;
+    TextView tv2;
+    Button btn_on;
+    Button btn_off;
     String CashCommandPath="CashCommand.txt";
-    public static final Logger logger = Logger.getLogger(MainActivity.class.getName());
-
+    Logger logger;
+    String ServerAddress ="http://192.168.1.106:8080";   // пример
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        try {
-            LogManager.getLogManager().readConfiguration(
-                    MainActivity.class.getResourceAsStream("com/example/iotapplication/logging.properties"));
-        } catch (IOException ignored) { }
-        logger.log(Level.INFO,"application start");
-    }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_on.setOnClickListener(v -> new OnOffSender().execute("on"));
+        tv=findViewById(R.id.textView);
+        tv2=findViewById(R.id.textView2);
+        btn_on = findViewById(R.id.button2);
+        btn_off = findViewById(R.id.button);
 
-        btn_off.setOnClickListener(v -> new OnOffSender().execute("off"));
+        logger = Logger.getLogger(MainActivity.class.getName());
+
+        try {
+            System.setProperty("java.util.logging.config.file",
+                    "logging.properties");
+            logger.log(Level.INFO,"logger is ready");
+        } catch (Exception e) {
+            logger.log(Level.SEVERE,"logger is not ready",e);
+        }
+        logger.log(Level.INFO,"application start");
+    }
+
+        btn_on.setOnClickListener(v -> SetCashCommand("on"));
+
+        btn_off.setOnClickListener(v -> SetCashCommand("on"));
 
         createNotificationChannel();
 
-    String command=GetCashCommand();
-        if (!command.equals("none")) new OnOffSender().execute(command);
+        ShowConnectionOff();
 
         RequestSender RC = new RequestSender();
         RC.execute();
     }
+
 private String GetCashCommand()
 {
-    FileInputStream fin = null;
     try {
-        fin = openFileInput(CashCommandPath);
+        FileInputStream fin = openFileInput(CashCommandPath);
         byte[] bytes = new byte[fin.available()];
         fin.read(bytes);
         fin.close();
@@ -82,32 +90,27 @@ private String GetCashCommand()
     }
     return "none";
 }
-private Boolean SetCashCommand(String command)
+private void SetCashCommand(String command)
 {
-    FileOutputStream fos = null;
+
     try {
-        fos = openFileOutput(CashCommandPath, MODE_PRIVATE);
+        FileOutputStream fos = openFileOutput(CashCommandPath, MODE_PRIVATE);
         fos.write(command.getBytes());
         fos.close();
-        return true;
     }
     catch(IOException ex) {
         logger.log(Level.SEVERE,"write file "+CashCommandPath,ex);
-        return false;
     }
-
 }
     private void ShowConnectionOff()
     {
         tv2.setText("Подключение отсутствует");
         tv2.setTextColor(Color.RED);
-        isConnection=false;
     }
     private void ShowConnectionOn()
     {
         tv2.setText("Подключено");
         tv2.setTextColor(Color.GREEN);
-        isConnection=true;
     }
 
     private void createNotificationChannel() {
@@ -124,53 +127,59 @@ private Boolean SetCashCommand(String command)
     }
 
     @SuppressLint("StaticFieldLeak")
-    сlass OnOffSender extends AsyncTask<String, Void, Void> {
-        @Override
-        protected Void doInBackground(String... strings) {
-        logger.log(Level.INFO,"try to send "+strings[0]);
-        if (!SetCashCommand(strings[0])) return null;
-
-            if (!isConnection) return null;
-            boolean endOperation=false;
-            while (!endOperation) {
-                try {
-                    String command=GetCashCommand();
-                    URL url = new URL(ServerAddress);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod("POST");
-                    con.setRequestProperty("Content-Type", "text/html");
-
-                    con.setDoOutput(true);
-                    DataOutputStream dStream = new DataOutputStream(con.getOutputStream());
-                    dStream.writeBytes(command);
-                    dStream.flush();
-                    dStream.close();
-
-                    ShowConnectionOn();
-                    endOperation=true;
-                    logger.log(Level.FINE,command+" sent");
-
-                } catch (Exception e) {
-                    logger.log(Level.SEVERE,"connection failure",e);
-                    ShowConnectionOff();
-                    try {TimeUnit.SECONDS.sleep(3);} catch (InterruptedException ignored) {}
-                }
-            }
-            return null;
-        }
-
-
-
-    @SuppressLint("StaticFieldLeak")
     class RequestSender extends AsyncTask<Void, String, Void> {
         @Override
         protected Void doInBackground(Void... voids)
         {
             while (true) {
+                String command=GetCashCommand();
+                if (!command.equals("none"))
+                {
+                    boolean endOperation=false;
+                    while (!endOperation) {
+                        try {
+                            command=GetCashCommand();
+                            logger.log(Level.INFO,"try to send command "+command);
+
+                            URL obj = new URL(ServerAddress);
+                            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                            con.setRequestMethod("POST");
+                            con.setRequestProperty("Content-Type", "text/html");
+                            con.setDoOutput(true);
+
+                            OutputStream os = con.getOutputStream();
+                            byte[] input = command.getBytes(StandardCharsets.UTF_8);
+                            os.write(input, 0, input.length);
+
+                            int r=con.getResponseCode();
+                            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                            String inputLine;
+                            StringBuilder data=new StringBuilder();
+                            while ((inputLine = in.readLine()) != null)
+                            {
+                                data.append(inputLine);
+                            }
+                            in.close();
+
+
+                            publishProgress("connection on");
+                            endOperation=true;
+
+                            logger.log(Level.INFO,"command "+command+" sent, code: "+ r +" data: "+data);
+                        } catch (Exception e) {
+                            logger.log(Level.SEVERE,"connection failure",e);
+                            publishProgress("connection off");
+                            try {TimeUnit.SECONDS.sleep(2);} catch (InterruptedException ignored) {}
+                        }
+                    }
+                }
+
                 StringBuilder data = new StringBuilder();;
                 try {
                     URL url = new URL(ServerAddress);
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setConnectTimeout(2000);
                     con.setRequestMethod("GET");
                     con.setRequestProperty("Content-Type", "text/html");
                     BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -179,9 +188,10 @@ private Boolean SetCashCommand(String command)
                         data.append(inputLine)
                     }
                     in.close();
-                    ShowConnectionOn();
+                    publishProgress("connection on");
                 } catch (Exception e) {
                     logger.log(Level.SEVERE,"connection failure",e);
+                    publishProgress("connection off");
                 }
                 publishProgress(data.toString());
 
@@ -193,23 +203,30 @@ private Boolean SetCashCommand(String command)
         protected void onProgressUpdate(String... messages) {
             super.onProgressUpdate(messages[0]);
 
+            switch (messages[0]) {
+                case "w":
+                    logger.log(Level.INFO, "alarm signal");
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "101")
+                            .setSmallIcon(R.drawable.signalicon)
+                            .setContentTitle("ВНИМАНИЕ")
+                            .setContentText("сработала сигнализация")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-            if (messages[0].equals("w")) {
-                logger.log(Level.INFO,"alarm signal");
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "101")
-                        .setSmallIcon(R.drawable.signalicon)
-                        .setContentTitle("ВНИМАНИЕ")
-                        .setContentText("сработала сигнализация")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                    id += 1;
+                    if (id > 200) id = 101;
 
-                id += 1;
-                if (id > 200) id = 101;
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+                    notificationManager.notify(id, builder.build());
 
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
-                notificationManager.notify(id, builder.build());
-
-                Date date = new Date();
-                tv.setText(date.toString());
+                    Date date = new Date();
+                    tv.setText(date.toString());
+                    break;
+                case "connection on":
+                    ShowConnectionOn();
+                    break;
+                case "connection off":
+                    ShowConnectionOff();
+                    break;
             }
         }
     }
