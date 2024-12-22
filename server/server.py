446b24d9d0b1676c21032f70efcd63ca206eff7c
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2024-12-22 23:03:58 +0500

    update do_GET

--- server/server.py
+++ server/server.py
@@ -1,15 +1,19 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
-import random
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    toclientmessage = ''
     # определяем метод `do_GET`
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        s=random.choice(["no","22.12.2024 12:00"])
-        print(s)
-        self.wfile.write(str.encode(s))
+        self.end_headers
+        print(self.toclientmessage)
+        if (self.toclientmessage == ''):
+            self.wfile.write(str.encode('no'))
+        else:
+            self.wfile.write(str.encode(self.toclientmessage))
+            toclientmessage = ''
     # определяем метод `do_POST`
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
