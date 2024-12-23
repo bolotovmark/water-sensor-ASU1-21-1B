c94fb95cf96a0c5801ce44664fb1da8e686154b8
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2024-12-23 17:31:16 +0500

    update send message to client

--- server/server.py
+++ server/server.py
@@ -1,22 +1,23 @@
-from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 
+toclientmessage=''
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    toclientmessage = ''
     # определяем метод `do_GET`
     def do_GET(self):
+        global toclientmessage
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
-        self.end_headers
-        print(self.toclientmessage)
-        if (self.toclientmessage == ''):
-            self.wfile.write(str.encode('no'))
+        self.end_headers()
+        print(toclientmessage)
+        if toclientmessage == '':
+            self.wfile.write(str.encode("empty"))
         else:
-            self.wfile.write(str.encode(self.toclientmessage))
-            self.toclientmessage = ''
+            self.wfile.write(str.encode("w"))
+            toclientmessage = ''
     # определяем метод `do_POST`
     def do_POST(self):
+        global toclientmessage
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
@@ -27,7 +28,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'Received: ')
         response.write(body)
         self.wfile.write(response.getvalue())
-        self.toclientmessage = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        toclientmessage = "notnull"
         print(body)
 httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
 httpd.serve_forever()
