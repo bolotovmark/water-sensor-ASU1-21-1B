aa125cf9d42e17484356fb02a6aabf7f34b16064
Ekaterina <92542464+Ekaterina-Tyutyunykh@users.noreply.github.com>
2024-12-27 09:16:17 +0500

    Modul server code and improve logging

--- server/server.py
+++ server/server.py
@@ -2,30 +2,21 @@ from http.server import HTTPServer, BaseHTTPRequestHandler  # класс
 import qrcode
 import random
 import socket
+import string
 import logging
 import io
 import qrcode
 from io import BytesIO  # класс
 import ssl
 
-logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
-
 
 def random_token():
-    str1 = '123456789'
-    str2 = 'qwertyuiopasdfghjklzxcvbnm'
-    str3 = str2.upper()
-    str4 = str1 + str2 + str3
-    ls = list(str4)
-    random.shuffle(ls)
+    DIGITS = '123456789'
+    ls = list(string.ascii_letters)
+    for i in DIGITS:
+        ls.append(i)
     psw = ''.join([random.choice(ls) for x in range(4)])
     return psw
-
-
-toclientmessage = ''
-frommobile = b''  # Embedded
-token = str(random_token())
 
 
 def get_local_ip():
@@ -35,35 +26,45 @@ def get_local_ip():
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    __logger = None
+    __token = None
+    __to_client_message = ''
+    __from_mobile = b''  # Embedded
 
+    @classmethod
+    def set_logger(cls, logger):
+        cls.__logger = logger
+
+    @classmethod
+    def set_token(cls, token):
+        cls.__token = token
     # определяем метод `do_GET`
     def do_GET(self):
-        global toclientmessage, frommobile
+        logger = SimpleHTTPRequestHandler.__logger
+        token = SimpleHTTPRequestHandler.__token
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        User_Agent = str(self.headers['User-Agent'])
-        User_Token = str(self.headers['token'])
-        if (User_Agent == 'mobile'):
-            if (token == User_Token):
-                # print(toclientmessage)
-                print("receive get from mobile")
-                if (toclientmessage == ''):
+        user_agent = str(self.headers['User-Agent'])
+        user_token = str(self.headers['token'])
+        if (user_agent == 'mobile'):
+            if (token == user_token):
+                # print(to_client_message )
+                logger.info("receive get from mobile")
+                if (SimpleHTTPRequestHandler.__to_client_message == ''):
                     self.wfile.write(str.encode("empty"))
                 else:
                     self.wfile.write(str.encode("w"))
-                    logging.info("Mobile User Request")
-                    toclientmessage = ''
+                    logger.info("Mobile User Request")
+                    SimpleHTTPRequestHandler.__to_client_message = ''
             else:
-                print("receive get from mobile with unknown token")
+                logger.info("receive get from mobile with unknown token")
                 self.send_response(403)
                 self.end_headers()
-        elif (User_Agent == 'Embedded'):
-            # if(frommobile)
-            print("receive get from Embedded")
-            print(frommobile)
-            self.wfile.write(frommobile)  # запрос включения/выключения от устройства
-            frommobile = b''
+        elif (user_agent == 'Embedded'):
+            logger.info("Unknown User")
+            self.wfile.write(SimpleHTTPRequestHandler.__from_mobile)  # запрос включения/выключения от устройства
+            SimpleHTTPRequestHandler.__from_mobile = b''
 
 
         else:
@@ -73,50 +74,58 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     # определяем метод `do_POST`
     def do_POST(self):
-        global toclientmessage, frommobile
+        logger = SimpleHTTPRequestHandler.__logger
+        token = SimpleHTTPRequestHandler.__token
         content_length = int(self.headers['Content-Length'])
-        User_Agent = str(self.headers['User-Agent'])
-        User_Token = str(self.headers['token'])
-
-        if (User_Agent == 'mobile'):
-            if (token == User_Token):
-                print("receive post from mobile")
-                frommobile = self.rfile.read(content_length)  # включение/выключение
-                logging.info(f"Token valide. Mobile User {frommobile} device")
+        user_agent = str(self.headers['User-Agent'])
+        user_token = str(self.headers['token'])
+        if (user_agent == 'mobile'):
+            if (token == user_token):
+                SimpleHTTPRequestHandler.__from_mobile = self.rfile.read(content_length)  # включение/выключение
+                logger.info(
+                    f"Receive post from mobile. Token valide. Mobile User {SimpleHTTPRequestHandler.__from_mobile} device")
                 self.send_response(200)
                 self.end_headers()
             else:
-                logging.info("Try connect. Token not valide")
-                print("receive post from mobile with unknown token")
+                logging.info("Try connect. Token not valide. Receive post\
+                                              from mobile with unknown token")
                 self.send_response(403)
                 self.end_headers()
-        elif (User_Agent == 'Embedded'):
-            print("receive post from Embedded")
+        elif (user_agent == 'Embedded'):
             body = self.rfile.read(content_length)
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(str.encode(""))
-            toclientmessage = "notnull"
-            logging.info("Alarm Signal")
+            SimpleHTTPRequestHandler.__to_client_message = "notnull"
+            logging.info("Receive post from Embedded. Alarm Signal")
             print(body)
         else:
             logging.info("Unknown User")
             self.send_response(400)
             self.end_headers()
 
+def main():
+    logger = logging.getLogger('logger')
+    logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
+    token = str(random_token())
+    data = f"https://{str(get_local_ip())}:8080-{token}"
+    qr = qrcode.QRCode()
+    qr.add_data(data)
+    f = io.StringIO()
+    qr.print_ascii(out=f)
+    f.seek(0)
+    print(f.read())
+    print(get_local_ip())
+    print(token)
+    SimpleHTTPRequestHandler.set_logger(logger)
+    SimpleHTTPRequestHandler.set_token(token)
+    httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
+    ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain('./app.crt',keyfile='./app.key')
+    httpd.socket = ssl_context.wrap_socket (httpd.socket, server_side=True)
+    httpd.serve_forever()
 
-data = "https://" + str(get_local_ip()) + ":8080-" + token
-qr = qrcode.QRCode()
-qr.add_data(data)
-f = io.StringIO()
-qr.print_ascii(out=f)
-f.seek(0)
-print(f.read())
-print(get_local_ip())
-print(token)
-httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
-ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain('./app.crt', keyfile='./app.key')
-httpd.socket = ssl_context.wrap_socket(httpd.socket, server_side=True)
-httpd.serve_forever()
+if __name__ == "__main__":
+    main()
